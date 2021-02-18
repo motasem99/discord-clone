0323413f829d809d8eae0bad575cb82f96ec9a91
mutasem <motasemimad10@gmail.com>
2021-02-18 15:35:47 +0200

    working on sidebar bottom

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Sidebar.css';
+import SidebarChannel from '../SidebarChannel/SidebarChannel';
 import ExpendMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CallIcon from '@material-ui/icons/Call';
 
 const Sidebar = () => {
   return (
@@ -15,6 +20,24 @@ const Sidebar = () => {
             <ExpendMoreIcon />
             <h4>Text Channel</h4>
           </div>
+          <AddIcon className='sidebar__addChannel' />
+        </div>
+        <div className='sidebar__channelsList'>
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+      <div className='sidebar__voice'>
+        <SignalCellularAltIcon className='voiceIcon' fontSize='large' />
+        <div className='sidebar__voiceInfo'>
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
+        </div>
+        <div className='sidebar__voiceIcons'>
+          <InfoOutlinedIcon />
+          <CallIcon />
         </div>
       </div>
     </div>
