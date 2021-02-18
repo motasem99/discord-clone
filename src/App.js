ab243e5de17f8049893ae3e81fe85e1354c29bac
mutasem <motasemimad10@gmail.com>
2021-02-18 16:37:09 +0200

    working on chatPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar.jsx';
+import Chat from './components/Chat/Chat.jsx';
 
 function App() {
   return (
     <div className='app'>
       <Sidebar />
+      <Chat />
     </div>
   );
 }
